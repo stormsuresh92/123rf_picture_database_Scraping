b57fe76f62947cff41f21ca337c00e096ace192f
stormsuresh92 <38948398+stormsuresh92@users.noreply.github.com>
2021-04-25 13:20:08 +0530

    Update 123rf_image_Scraping.py

--- 123rf_image_Scraping.py
+++ 123rf_image_Scraping.py
@@ -1,23 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-import os
+
 
 headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'}
 
-def get_images(url, folder):
-    os.mkdir(os.path.join(os.getcwd(), folder))
-    os.chdir(os.path.join(os.getcwd(), folder))
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    tags = soup.find_all('img', {'class' : 'ImageThumbnail__image'})
-    for tag in tags:
-        title = tag.get('alt')
-        href_link = tag.get('src').replace('?ver=6', '')
-     
-        with open(title.replace(' ', '_').replace('/', '').replace(',', '') + '.jpg', 'wb') as f:
+url = 'https://www.123rf.com/stock-photo/dna.html?page=2sti=n0r30i4cahs4fsdzhv%7C'
+res = requests.get(url, headers=headers)
+soup = BeautifulSoup(res.text, 'html.parser')
+tags = soup.find_all('img', {'class' : 'ImageThumbnail__image'})
+for tag in tags:
+    title = tag.get('alt')
+    href_link = tag.get('src').replace('?ver=6', '')
+    
+    try: 
+        with open(title.replace(' ', '_').replace('/', '').replace(',', '').replace('.', '') + '.jpg', 'wb') as f:
             out = requests.get(href_link)
             f.write(out.content)
             print('Downloaded..')
-
-get_images('https://www.123rf.com/stock-photo/dna.html?page=2sti=n0r30i4cahs4fsdzhv%7C', 'output_images')
+    except:
+        pass
