3edd4baab32b7219e370deb7d3b604af08c4a35b
stormsuresh92 <38948398+stormsuresh92@users.noreply.github.com>
2021-04-25 12:57:25 +0530

    Update 123rf_image_Scraping.py

--- 123rf_image_Scraping.py
+++ 123rf_image_Scraping.py
@@ -5,20 +5,19 @@ import os
 
 headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'}
 
-url = 'https://www.123rf.com/stock-photo/dna.html?page=2sti=n0r30i4cahs4fsdzhv%7C'
-res = requests.get(url, headers=headers)
-soup = BeautifulSoup(res.text, 'html.parser')
-tags = soup.find_all('img', {'class' : 'ImageThumbnail__image'})
-for tag in tags:
-    title = tag.get('alt')
-    href_link = tag.get('src').replace('?ver=6', '')
+def get_images(url, folder):
+    os.mkdir(os.path.join(os.getcwd(), folder))
+    os.chdir(os.path.join(os.getcwd(), folder))
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    tags = soup.find_all('img', {'class' : 'ImageThumbnail__image'})
+    for tag in tags:
+        title = tag.get('alt')
+        href_link = tag.get('src').replace('?ver=6', '')
      
-    with open(title.replace(' ', '_').replace('/', '').replace(',', '') + '.jpg', 'wb') as f:
-         out = requests.get(href_link)
-         f.write(out.content)
-         print('Downloaded..')
+        with open(title.replace(' ', '_').replace('/', '').replace(',', '') + '.jpg', 'wb') as f:
+            out = requests.get(href_link)
+            f.write(out.content)
+            print('Downloaded..')
 
-
-
-
-
+ get_images('https://www.123rf.com/stock-photo/dna.html?page=2sti=n0r30i4cahs4fsdzhv%7C', 'output_images')
